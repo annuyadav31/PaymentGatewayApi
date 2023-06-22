56c5bba4ad3091fb295c6d65637b252534429d73
annuyadav31 <annuyadav99539@gmail.com>
2023-06-22 14:01:46 +0100

    Added Controller Tests

---
+++ tests/PaymentGatewaySolution.Api.Tests/PaymentControllerTests/AddPaymentControllerTests.cs
@@ -0,0 +1,64 @@
+﻿using AutoFixture;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Moq;
+using PaymentGateway.Core.ModelDTO.PaymentDTO;
+using PaymentGatewaySolution.Api.Controllers.PaymentController;
+using PaymentGatewaySolution.Core.ServiceContracts.IPaymentService;
+using Xunit.Abstractions;
+using Xunit.Sdk;
+
+namespace PaymentGatewaySolution.Api.Tests.PaymentControllerTests
+{
+    public class AddPaymentControllerTests
+    {
+        #region "ReadOnly Variables"
+        private readonly Fixture _fixture;
+        private readonly AddPaymentController _addPaymentController;
+        private readonly ITestOutputHelper _testOutputHelper;
+        private readonly IProcessPaymentService _processPaymentService;
+        private readonly Mock<IProcessPaymentService> _processPaymentServiceMock;
+        private readonly Mock<ILogger<AddPaymentController>> _loggerMock;
+
+        #endregion
+        public AddPaymentControllerTests(ITestOutputHelper testOutputHelper)
+        {
+            _processPaymentServiceMock = new Mock<IProcessPaymentService>();
+            _processPaymentService = _processPaymentServiceMock.Object;
+            _loggerMock = new Mock<ILogger<AddPaymentController>>();
+            _addPaymentController = new AddPaymentController(_loggerMock.Object, _processPaymentService);
+            _fixture = new Fixture();
+            _testOutputHelper = testOutputHelper;
+        }
+        [Fact]
+        public async Task ProcessPayment_ValidRequest_ReturnsOk()
+        {
+            // Arrange
+            var paymentRequest = _fixture.Create<AddPaymentRequestDTO>();
+            var paymentResponse = _fixture.Create<PaymentResponse>();
+            _processPaymentServiceMock.Setup(x => x.ProcessPayment(paymentRequest)).ReturnsAsync(paymentResponse);
+
+            // Act
+            var result = await _addPaymentController.ProcessPayment(paymentRequest);
+
+            // Assert
+            Assert.IsType<OkObjectResult>(result.Result);
+            var okResult = (OkObjectResult)result.Result;
+            Assert.Equal(paymentResponse, okResult.Value);
+        }
+
+        [Fact]
+        public async Task ProcessPayment_WithInvalidPaymentRequest_ReturnsBadRequest()
+        {
+            // Arrange
+            AddPaymentRequestDTO paymentRequest = _fixture.Create<AddPaymentRequestDTO>();
+            paymentRequest.CardDetails.CardHolderName = null;
+
+            // Act
+            var result = await _addPaymentController.ProcessPayment(paymentRequest);
+
+            // Assert
+            Assert.IsType<BadRequestObjectResult>(result.Result);
+        }
+    }
+}
---
+++ tests/PaymentGatewaySolution.Api.Tests/PaymentControllerTests/GetPaymentControllerTests.cs
@@ -0,0 +1,65 @@
+﻿using AutoFixture;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Moq;
+using PaymentGateway.Core.ModelDTO.PaymentDTO;
+using PaymentGatewaySolution.Api.Controllers.PaymentController;
+using PaymentGatewaySolution.Core.ServiceContracts.IPaymentService;
+using Xunit.Abstractions;
+using FluentAssertions;
+
+namespace PaymentGatewaySolution.Api.Tests.PaymentControllerTests
+{
+    public class GetPaymentControllerTests
+    {
+        #region "ReadOnly Variables"
+        private readonly Fixture _fixture;
+        private readonly GetPaymentDetailsController _getPaymentController;
+        private readonly ITestOutputHelper _testOutputHelper;
+        private readonly IGetPaymentDetailsService _getPaymentService;
+        private readonly Mock<IGetPaymentDetailsService> _getPaymentServiceMock;
+        private readonly Mock<ILogger<GetPaymentDetailsController>> _loggerMock;
+
+        #endregion
+        public GetPaymentControllerTests(ITestOutputHelper testOutputHelper)
+        {
+            _getPaymentServiceMock = new Mock<IGetPaymentDetailsService>();
+            _getPaymentService = _getPaymentServiceMock.Object;
+            _loggerMock = new Mock<ILogger<GetPaymentDetailsController>>();
+            _getPaymentController = new GetPaymentDetailsController(_loggerMock.Object, _getPaymentService);
+            _fixture = new Fixture();
+            _testOutputHelper = testOutputHelper;
+        }
+
+        [Fact]
+        public async Task GetPaymentDetails_WithValidTransactionId_ReturnsOk()
+        {
+            var transactionId = _fixture.Create<Guid>();
+            var paymentDetails = _fixture.Create<PaymentResponse>();
+            _getPaymentServiceMock.Setup(x => x.GetPaymentDetails(transactionId)).ReturnsAsync(paymentDetails);
+
+            // Act
+            var result = await _getPaymentController.GetPaymentDetails(transactionId.ToString());
+
+            // Assert
+            Assert.IsType<OkObjectResult>(result.Result);
+            var okResult = (OkObjectResult)result.Result;
+            Assert.Equal(paymentDetails, okResult.Value);
+        }
+
+        [Fact]
+        public async Task GetPaymentDetails_WithInvalidTransactionId_ReturnsNull()
+        {
+            // Arrange
+            Guid paymentId = Guid.NewGuid();
+
+            // Act
+            var result = await _getPaymentController.GetPaymentDetails(paymentId.ToString());
+
+            // Assert
+            Assert.IsType<OkObjectResult>(result.Result);
+            var okResult = (OkObjectResult)result.Result;
+            okResult.Value.Should().BeNull();
+        }
+    }
+}
