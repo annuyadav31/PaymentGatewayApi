3769fedd1df9b22036e163101f10314d44f2b663
annuyadav31 <annuyadav99539@gmail.com>
2023-06-22 09:36:10 +0100

    Added CardDetails and Payment Model to Domain Model

---
+++ src/PaymentGatewaySolution.Core/Domain/Models/CardDetails.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace PaymentGatewaySolution.Core.Domain.Models
+{
+    /// <summary>
+    /// Card Details Domain Model Class
+    /// </summary>
+    /// 
+
+    [Table("CardDetails")]
+    public class CardDetails
+    {
+        [Key]
+        public Guid CardID { get; set; }
+
+        [StringLength(19)]
+        public string? CardNumber { get; set; }
+
+        [StringLength(100)]
+        public string? CardHolderName { get; set; }
+
+        [Range(1, 12)]
+        public int ExpiryMonth { get; set; }
+
+        [Range(2022, 2100)]
+        public int ExpiryYear { get; set; }
+
+        [Range(3, 4)]
+        public int Cvv { get; set; }
+    }
+}
---
+++ src/PaymentGatewaySolution.Core/Domain/Models/Payment.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace PaymentGatewaySolution.Core.Domain.Models
+{
+    /// <summary>
+    /// Payment Domain Model Class
+    /// </summary>
+    /// 
+
+    [Table("Payments")]
+    public class Payment
+    {
+        //Primary Key
+        [Key]
+        public Guid TransactionID { get; set; }
+
+        [Range(0, double.MaxValue)]
+        public double Amount { get; set; }
+
+        [StringLength(3)]
+        public string? Currency { get; set; }
+
+        public DateTime CreatedOn { get; set; }
+
+        public bool Successful { get; set; }
+
+        [StringLength(500)]
+        public string? Message { get; set; }
+
+        //Foreign Key
+        public Guid CardID { get; set; }
+
+        [ForeignKey(nameof(CardID))]
+        public CardDetails? CardDetails { get; set; }
+
+    }
+}
