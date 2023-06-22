6d3464c6fad1d8aea345b42216ecffafcba05e6e
annuyadav31 <annuyadav99539@gmail.com>
2023-06-22 13:04:44 +0100

    Added Test Cases for GetPaymentDetails Service

--- src/PaymentGatewaySolution.Core/Services/PaymentService/GetPaymentDetailsService.cs
+++ src/PaymentGatewaySolution.Core/Services/PaymentService/GetPaymentDetailsService.cs
@@ -1,10 +1,29 @@
-﻿using PaymentGateway.Core.ModelDTO.PaymentDTO;
+﻿using Microsoft.Extensions.Logging;
+using Moq;
+using PaymentGateway.Core.Domain.BankSimulatorContracts;
+using PaymentGateway.Core.ModelDTO.PaymentDTO;
+using PaymentGatewaySolution.Core.Domain.RepositoryContracts.PaymentDetailsContracts;
 using PaymentGatewaySolution.Core.ServiceContracts.IPaymentService;
 
 namespace PaymentGatewaySolution.Core.Services.PaymentService
 {
     public class GetPaymentDetailsService : IGetPaymentDetailsService
     {
+        #region "Private Readonly Fields"
+        private readonly IGetPaymentDetailsRepository _getPaymentDetailsRepository;
+        private readonly IBankSimulator _bankSimulator;
+        private readonly Mock<IBankSimulator> _bankSimulatorMock;
+        private readonly ILogger<GetPaymentDetailsService> _logger;
+        #endregion
+
+        public GetPaymentDetailsService(IGetPaymentDetailsRepository getPaymentDetailsRepository, IBankSimulator bankSimulator, ILogger<GetPaymentDetailsService> logger)
+        {
+            _getPaymentDetailsRepository = getPaymentDetailsRepository;
+            _bankSimulator = bankSimulator;
+            _logger = logger;
+            _bankSimulatorMock = new Mock<IBankSimulator>();
+            _bankSimulator = _bankSimulatorMock.Object;
+        }
         public Task<PaymentResponse> GetPaymentDetails(Guid transactionId)
         {
             throw new NotImplementedException();
---
+++ tests/PaymentGatewaySolution.Core.Tests/PaymentServiceTests/GetPaymentDetailsServiceTests.cs
@@ -0,0 +1,62 @@
+﻿namespace PaymentGatewaySolution.Core.Tests.PaymentServiceTests
+{
+    public class GetPaymentDetailsServiceTests
+    {
+        #region "Private Readonly Fields"
+        private readonly IFixture _fixture;
+        private readonly ITestOutputHelper _testOutputHelper;
+        private readonly IBankSimulator _bankSimulator;
+        private readonly Mock<IBankSimulator> _bankSimulatorMock;
+        private readonly IGetPaymentDetailsService _getPaymentDetailsService;
+        private readonly IGetPaymentDetailsRepository _getPaymentDetailsRepository;
+        private readonly Mock<IGetPaymentDetailsRepository> _getPaymentDetailsRepositoryMock;
+        private readonly ILogger<GetPaymentDetailsService> _logger;
+        #endregion
+
+        public GetPaymentDetailsServiceTests(ITestOutputHelper testOutputHelper)
+        {
+            _fixture = new Fixture();
+            _testOutputHelper = testOutputHelper;
+            _getPaymentDetailsRepositoryMock = new Mock<IGetPaymentDetailsRepository>();
+            _getPaymentDetailsRepository = _getPaymentDetailsRepositoryMock.Object;
+            _bankSimulatorMock = new Mock<IBankSimulator>();
+            _bankSimulator = _bankSimulatorMock.Object;
+            _logger = Mock.Of<ILogger<GetPaymentDetailsService>>();
+            _getPaymentDetailsService = new GetPaymentDetailsService(_getPaymentDetailsRepository, _bankSimulator, _logger);
+        }
+
+        [Fact]
+        public async Task GetPaymentDetails_Should_Return_PaymentDetails_When_Payment_Exists()
+        {
+            // Arrange
+            var paymentRequest = _fixture.Create<AddPaymentRequestDTO>();
+            Payment payment = paymentRequest.ToPayment();
+            payment.TransactionID = new Guid();
+
+            _getPaymentDetailsRepositoryMock.Setup(x => x.GetPayment(payment.TransactionID)).ReturnsAsync(payment);
+
+            // Act
+            var result = await _getPaymentDetailsService.GetPaymentDetails(payment.TransactionID);
+
+            // Assert
+            result.Should().NotBeNull();
+            result.TransactionID.Should().Be(payment.TransactionID);
+            result.CardNumber.Should().Contain("***");
+            result.CardHolderName.Should().Be(payment.CardDetails?.CardHolderName);
+            result.Successful.Should().Be(payment.Successful);
+        }
+
+        [Fact]
+        public async Task GetPaymentDetails_Should_Throw_PaymentGatewayException_When_Payment_Not_Found()
+        {
+            //Arrange
+            Guid paymentId = new Guid();
+
+            //Act
+            PaymentResponse paymentResponse = await _getPaymentDetailsService.GetPaymentDetails(paymentId);
+
+            //Assert
+            paymentResponse.Should().BeNull();
+        }
+    }
+}
